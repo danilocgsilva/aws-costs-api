8964085cabfe687ad9f21e18c88f9d93a000f24d
Danilo Silva <danilocgsilva@gmail.com>
2021-02-12 23:05:47 -0300

    Already touching boto3

--- aws_costs_api/AWSCosts.py
+++ aws_costs_api/AWSCosts.py
@@ -1,4 +1,10 @@
+import boto3
+import datetime
+from datetime import timedelta
+
 class AWSCosts:
 
     def getCosts(self):
-        return "to be implemented"
+        now = datetime.datetime.now()
+        client = boto3.client("ce")
+        return client.get_cost_and_usage()
