3bb950929680c411a4acbe0cccb4500f2b3d8182
Danilo Silva <danilocgsilva@gmail.com>
2022-04-16 10:27:12 -0300

    Fix tests

--- tests/test_AWSCosts.py
+++ tests/test_AWSCosts.py
@@ -17,5 +17,11 @@ class test_AWSCosts(unittest.TestCase):
     def test_default_output_from_class(self):
         awsCosts = AWSCosts(CostExplorerMock())
         rawResults = awsCosts.getCosts()
-        print(rawResults)
+
+        amount0 = rawResults["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        amount1 = rawResults["ResultsByTime"][1]["Total"]["BlendedCost"]["Amount"]
+        amount2 = rawResults["ResultsByTime"][2]["Total"]["BlendedCost"]["Amount"]
+        self.assertEqual("0.821", amount0[:5])
+        self.assertEqual("0.821", amount1[:5])
+        self.assertEqual("0.820", amount2[:5])
 
